055b4004914d31ae2c04b9bd0e1f95f9235ed6ba
azizafarooq <azizafarooq2000@gmail.com>
2024-01-08 14:22:49 +0100

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+const passwordInsicurissima = document.getElementById('main-title');
+
+const nomeUtente = prompt("Qual è il tuo nome?");
+console.log(nomeUtente);
+
+const cognomeUtente = prompt("Qual è il tuo cognome?");
+console.log(cognomeUtente);
+
+const coloreUtente = prompt("Qual è il tuo colore preferito?");
+console.log(coloreUtente);
+
+const resultElement = nomeUtente + cognomeUtente +
+coloreUtente + "23";
+console.log(resultElement);
+
+passwordInsicurissima.innerText = "La tua password è " + resultElement;
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Esercizio di oggi: (insicurissimo) password generator
+
+# Consegna esercizio
+ Chiedi all'utente il suo nome,
+ poi chiedi il suo cognome,
+ poi chiedi il suo colore preferito
+ Infine scrivi sulla pagina nomecognomecolorepreferito23
+
+# Svolgimento
+ -Chiedere all'utente il suo nome
+ -Chiedere all'utente il suo cognome
+ -Chiedere all'utente il suo colore preferito
+ -Stampare i tre dati richiesti precedentemente concatenandoli
+
+   
